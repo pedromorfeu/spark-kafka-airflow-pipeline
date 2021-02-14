3622e7bbec519568812dc73fa0648f2da2db25a5
pedromorfeu <pedromorfeu@gmail.com>
2021-02-14 19:01:16 +0100

    added sources union

--- src/main/scala/com/pipeline/PiepelineMain.scala
+++ src/main/scala/com/pipeline/PiepelineMain.scala
@@ -4,7 +4,7 @@ import com.pipeline.metadata.model.{DataflowMetadata, DataflowTransformationVali
 import net.liftweb.json.{DefaultFormats, JValue}
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{array, array_union, col, expr, lit, when}
+import org.apache.spark.sql.functions._
 
 import scala.io.Source
 
@@ -34,65 +34,65 @@ object PiepelineMain {
         // dataflow processing
         logger.info(s"processing dataflow ${dataflow.name}")
 
-        dataflow.sources.foreach(source => {
-          logger.info(s"processing source ${source.name}")
-
-          // source processing
-          val inputPath = source.path
-          logger.info(s"reading $inputPath")
-          var dataframe = session.read.json(inputPath)
-          dataframe.show
-
-          // add fields
-          dataflow.transformations
-            .filter(_.`type` == "add_fields")
-            .foreach(transformation => {
-              logger.info(s"applying transformation ${transformation.name}")
-
-              // TODO: use foldLeft: columns.foldLeft(df)((acc, col) => acc.withColumn(col, acc("incipit").as[String].contains(col)))
-              transformation.params.addFields.get
-                .foreach(field => dataframe = dataframe.withColumn(field.name, expr(field.function)))
+        var dataframe =
+          dataflow.sources
+            .map(source => {
+              logger.info(s"source ${source.name}")
+              val inputPath = source.path
+              logger.info(s"reading $inputPath")
+              session.read.format(source.format).load(inputPath)
             })
+            .reduce(_ union _)
 
-          // validate fields
-          val conditions = dataflow.transformations
-            .filter(_.`type` == "validate_fields")
-            .flatMap(transformation => {
-              logger.info(s"applying transformation ${transformation.name}")
+        // add fields
+        dataflow.transformations
+          .filter(_.`type` == "add_fields")
+          .foreach(transformation => {
+            logger.info(s"applying transformation ${transformation.name}")
 
-              // transformation
-              logger.info("validating fields")
-              val validations: List[DataflowTransformationValidation] = transformation.params.validations.get
+            // TODO: use foldLeft: columns.foldLeft(df)((acc, col) => acc.withColumn(col, acc("incipit").as[String].contains(col)))
+            transformation.params.addFields.get
+              .foreach(field => dataframe = dataframe.withColumn(field.name, expr(field.function)))
+          })
 
-              validations
-                .flatMap(validation => {
-                  validation.validations
-                    .map {
-                      case "notEmpty" => (s"${validation.field} != ''", s"${validation.field}-notEmpty")
-                      case "notNull" => (s"${validation.field} is not null", s"${validation.field}-notNull")
-                    }
-                })
-            })
+        // validate fields
+        val conditions = dataflow.transformations
+          .filter(_.`type` == "validate_fields")
+          .flatMap(transformation => {
+            logger.info(s"applying transformation ${transformation.name}")
 
-          logger.info(s"conditions: $conditions")
-          val okDF = dataframe.filter(conditions.map(_._1).mkString(" and "))
-          okDF.show(false)
+            // transformation
+            logger.info("validating fields")
+            val validations: List[DataflowTransformationValidation] = transformation.params.validations.get
 
-          var notOkDF = dataframe
-            .filter(s"!(${conditions.map(_._1).mkString(" and ")})")
-            .withColumn("arraycoderrorbyfield", array())
+            validations
+              .flatMap(validation => {
+                validation.validations
+                  .map {
+                    case "notEmpty" => (s"${validation.field} != ''", s"${validation.field}-notEmpty")
+                    case "notNull" => (s"${validation.field} is not null", s"${validation.field}-notNull")
+                  }
+              })
+          })
 
-          conditions
-            .foreach(condition => {
-              notOkDF = notOkDF
-                .withColumn("arraycoderrorbyfield",
-                  when(expr(s"!${condition._1}"), array_union(col("arraycoderrorbyfield"), lit(Array(condition._2))))
-                    .otherwise(col("arraycoderrorbyfield")))
-            })
+        logger.info(s"conditions: $conditions")
+        val okDF = dataframe.filter(conditions.map(_._1).mkString(" and "))
+        okDF.show(false)
 
-          notOkDF.show(false)
+        var notOkDF = dataframe
+          .filter(s"!(${conditions.map(_._1).mkString(" and ")})")
+          .withColumn("arraycoderrorbyfield", array())
 
-        })
+        conditions
+          .foreach(condition => {
+            notOkDF = notOkDF
+              .withColumn("arraycoderrorbyfield",
+                when(expr(s"!${condition._1}"), array_union(col("arraycoderrorbyfield"), lit(Array(condition._2))))
+                  .otherwise(col("arraycoderrorbyfield")))
+          })
+
+        notOkDF.show(false)
+
       })
   }
 

645631d9175e30be57427709524be6ea1bf719fc
pedromorfeu <pedromorfeu@gmail.com>
2021-02-20 20:43:15 +0100

    added script to launch hdfs, kafka, airflow

---
+++ src/main/resources/start-env.sh
@@ -0,0 +1,15 @@
+# start hdfs
+/Users/pedromorfeu/hadoop/hadoop-2.10.1/start.sh
+
+# start spark
+
+
+# start airflow
+
+
+# kafka
+/Users/pedromorfeu/kafka/kafka_2.12-2.7.0/start-zookeeper.sh
+/Users/pedromorfeu/kafka/kafka_2.12-2.7.0/start-kafka.sh
+
+/Users/pedromorfeu/kafka/kafka_2.12-2.7.0/read-pipeline-ok.sh
+/Users/pedromorfeu/kafka/kafka_2.12-2.7.0/read-pipeline-ko.sh

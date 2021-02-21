2720a33b956e2fd8d4fa675f4d181dae3c644e9b
pedromorfeu <pedromorfeu@gmail.com>
2021-02-21 16:42:18 +0100

    split pipeline into multiple methods

--- src/main/scala/com/pipeline/PipelineProcessor.scala
+++ src/main/scala/com/pipeline/PipelineProcessor.scala
@@ -1,7 +1,7 @@
 package com.pipeline
 
 import com.pipeline.PiepelineMain.logger
-import com.pipeline.metadata.model.{Dataflow, DataflowMetadata, DataflowSource, DataflowTransformationValidation}
+import com.pipeline.metadata.model._
 import net.liftweb.json.{DefaultFormats, JValue}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -20,64 +20,82 @@ class PipelineProcessor(session: SparkSession, metadataPath: String, kafkaServer
         // dataflow processing
         logger.info(s"processing dataflow ${dataflow.name}")
 
-        // read sources
         val sources = dataflow.sources
-        var dataframe = readSources(session, sources)
-
-        // add fields
-        val transformations = dataflow.transformations
-        transformations
+        val transformations: Seq[DataflowTransformation] = dataflow.transformations
+        val fieldsToAdd = transformations
           .filter(_.`type` == "add_fields")
-          .foreach(transformation => {
-            logger.info(s"applying transformation ${transformation.name}")
 
-            // TODO: use foldLeft: columns.foldLeft(df)((acc, col) => acc.withColumn(col, acc("incipit").as[String].contains(col)))
-            transformation.params.addFields.get
-              .foreach(field => dataframe = dataframe.withColumn(field.name, expr(field.function)))
-          })
+        // read sources
+        var dataframe = readSources(session, sources)
+          // add fields
+          .transform(addFields(fieldsToAdd))
 
         // validate fields
-        val conditions = transformations
+        val fieldsToValidate = transformations
           .filter(_.`type` == "validate_fields")
-          .flatMap(transformation => {
-            logger.info(s"applying transformation ${transformation.name}")
-
-            // transformation
-            logger.info("validating fields")
-            val validations: List[DataflowTransformationValidation] = transformation.params.validations.get
-
-            validations
-              .flatMap(validation => {
-                validation.validations
-                  .map {
-                    case "notEmpty" => (s"${validation.field} != ''", s"${validation.field}-notEmpty")
-                    case "notNull" => (s"${validation.field} is not null", s"${validation.field}-notNull")
-                  }
-              })
-          })
-
-        logger.info(s"conditions: $conditions")
-        val okDF = dataframe.filter(conditions.map(_._1).mkString(" and "))
-        okDF.show(false)
-
-        var notOkDF = dataframe
-          .filter(s"!(${conditions.map(_._1).mkString(" and ")})")
-          .withColumn("arraycoderrorbyfield", array())
-
-        conditions
-          .foreach(condition => {
-            notOkDF = notOkDF
-              .withColumn("arraycoderrorbyfield",
-                when(expr(s"!${condition._1}"), array_union(col("arraycoderrorbyfield"), lit(Array(condition._2))))
-                  .otherwise(col("arraycoderrorbyfield")))
-          })
-
-        notOkDF.show(false)
+        val (okDF, koDF) = validateFields(fieldsToValidate, dataframe)
 
         // persist
-        persist(dataflow, okDF, notOkDF, kafkaServer)
+        persist(dataflow.sinks, okDF, koDF, kafkaServer)
 
       })
+  }
+
+  def validateFields(fieldsToValidate: Seq[DataflowTransformation], dataframe: DataFrame): (DataFrame, DataFrame) = {
+    // validate fields
+    val conditions = fieldsToValidate
+      .flatMap(transformation => {
+        logger.info(s"applying transformation ${transformation.name}")
+
+        // transformation
+        logger.info("validating fields")
+        val validations: List[DataflowTransformationValidation] = transformation.params.validations.get
+
+        validations
+          .flatMap(validation => {
+            validation.validations
+              .map {
+                case "notEmpty" => (s"${validation.field} != ''", s"${validation.field}-notEmpty")
+                case "notNull" => (s"${validation.field} is not null", s"${validation.field}-notNull")
+              }
+          })
+      })
+
+    logger.info(s"conditions: $conditions")
+    val okDF = dataframe.filter(conditions.map(_._1).mkString(" and "))
+    okDF.show(false)
+
+    var notOkDF = dataframe
+      .filter(s"!(${conditions.map(_._1).mkString(" and ")})")
+      .withColumn("arraycoderrorbyfield", array())
+
+    conditions
+      .foreach(condition => {
+        notOkDF = notOkDF
+          .withColumn("arraycoderrorbyfield",
+            when(expr(s"!${condition._1}"), array_union(col("arraycoderrorbyfield"), lit(Array(condition._2))))
+              .otherwise(col("arraycoderrorbyfield")))
+      })
+
+    notOkDF.show(false)
+
+    (okDF, notOkDF)
+  }
+
+  def addFields(fieldsToAdd: Seq[DataflowTransformation])(df: DataFrame): DataFrame = {
+    var dataframe = df
+
+    // add fields
+    fieldsToAdd
+      .foreach(transformation => {
+        logger.info(s"applying transformation ${transformation.name}")
+
+        // TODO: use foldLeft: columns.foldLeft(df)((acc, col) => acc.withColumn(col, acc("incipit").as[String].contains(col)))
+        transformation.params.addFields.get
+          .foreach(field => dataframe = dataframe.withColumn(field.name, expr(field.function)))
+      })
+
+    dataframe
   }
 
   def readSources(session: SparkSession, sources: List[DataflowSource]): DataFrame =
@@ -90,9 +108,9 @@ class PipelineProcessor(session: SparkSession, metadataPath: String, kafkaServer
       })
       .reduce(_ union _)
 
-  def persist(dataflow: Dataflow, okDF: DataFrame, notOkDF: DataFrame, kafkaServer: String): Unit =
+  def persist(sinks: Seq[DataflowSink], okDF: DataFrame, koDF: DataFrame, kafkaServer: String): Unit =
     // persist
-    dataflow.sinks
+    sinks
       .foreach(sink => {
 
         sink.input match {
@@ -106,34 +124,32 @@ class PipelineProcessor(session: SparkSession, metadataPath: String, kafkaServer
                   .save(path + "/" + sink.name))
 
             // publish to kafka
-            okDF
-              .select(to_json(struct("*")).as("value"))
-              .write
-              .format("kafka")
-              .option("kafka.bootstrap.servers", kafkaServer)
-              .option("topic", "pipeline-ok")
-              .save()
+            publishToKafka(okDF, kafkaServer, "pipeline-ok")
 
           case "validation_ko" =>
             sink.paths
               .foreach(path =>
-                notOkDF
+                koDF
                   .write
                   .mode(sink.saveMode)
                   .format(sink.format)
                   .save(path + "/" + sink.name))
 
             // publish to kafka
-            notOkDF
-              .select(to_json(struct("*")).as("value"))
-              .write
-              .format("kafka")
-              .option("kafka.bootstrap.servers", kafkaServer)
-              .option("topic", "pipeline-ko")
-              .save()
+            publishToKafka(koDF, kafkaServer, "pipeline-ko")
         }
 
       })
+
+  def publishToKafka(okDF: DataFrame, kafkaServer: String, topicOk: String): Unit = {
+    okDF
+      .select(to_json(struct("*")).as("value"))
+      .write
+      .format("kafka")
+      .option("kafka.bootstrap.servers", kafkaServer)
+      .option("topic", topicOk)
+      .save()
+  }
 
   def readMetadata(metadataPath: String): DataflowMetadata = {
     val metadataSource = Source.fromFile(metadataPath)
